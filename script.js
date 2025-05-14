0804a1c2d09df280686ab0328fec5402a579982d
NansyahTs <nansyahsan@gmail.com>
2025-05-14 19:27:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-// Hamburger toggle
-const menuToggle = document.getElementById('menuToggle');
-const menu = document.getElementById('menu');
+// JavaScript untuk men-toggle visibility dari menu navigasi
+const toggleNavButton = document.getElementById('toggleNav');
+const navMenu = document.getElementById('navMenu');
 
-menuToggle.addEventListener('click', () => {
-  menu.classList.toggle('show'); // Menampilkan atau menyembunyikan menu saat tombol diklik
-});
-
-// Smooth scroll
-document.querySelectorAll('.menu a').forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault(); // Mencegah link default behavior (scrolling ke anchor)
-    const section = document.querySelector(this.getAttribute('href')); // Mencari section yang sesuai dengan href
-    if (section) {
-      section.scrollIntoView({ behavior: 'smooth' }); // Scroll ke bagian dengan smooth animation
-    }
-  });
+// Ketika tombol "Menu" di-klik, toggle menu navigasi
+toggleNavButton.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
 });
