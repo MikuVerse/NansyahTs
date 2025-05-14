035484d31526170608f2cf26131ea95689d1cff2
NansyahTs <nansyahsan@gmail.com>
2025-05-14 20:01:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// JavaScript untuk men-toggle visibility dari menu navigasi
-const toggleNavButton = document.getElementById('toggleNav');
+// Hamburger toggle
+const toggleNav = document.getElementById('toggleNav');
 const navMenu = document.getElementById('navMenu');
 
-// Ketika tombol "Menu" di-klik, toggle menu navigasi
-toggleNavButton.addEventListener('click', () => {
+toggleNav.addEventListener('click', () => {
   navMenu.classList.toggle('show');
 });
