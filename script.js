5c5e4dcca5ce7da324c46879a18bbe5c86968650
NansyahTs <nansyahsan@gmail.com>
2025-05-14 18:49:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Hamburger toggle
+const menuToggle = document.getElementById('menuToggle');
+const menu = document.getElementById('menu');
+
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('show');
+});
+
 // Smooth scroll
 document.querySelectorAll('.menu a').forEach(link => {
   link.addEventListener('click', function(e) {
